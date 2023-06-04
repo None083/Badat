886c5348f47dbde0ce3c865e2e87726434f49695
Noelia <npergon914@g.educaand.es>
2023-06-04 12:15:20 +0200

    040623

--- tema 10/tulipArtJ.sql
+++ tema 10/tulipArtJ.sql
@@ -1,32 +1,56 @@
+-- Creo las tablas
+
 create table etiquetas(
 	idetiqueta integer,
 	nombre character varying(60),
 	constraint pk_etiquetas primary key (idetiqueta)
 );
 
-drop table materiales if exists;
 create table materiales(
-	idetiqueta integer,
+	idmaterial integer,
 	descripcion character varying(50),
 	constraint pk_materiales primary key (idmaterial)
 )inherits(etiquetas);
 
-drop table estilos if exists;
 create table estilos(
-	idetiqueta integer,
+	idestilo integer,
 	descripcion character varying (50),
 	constraint pk_estilos primary key (idestilo)
 )inherits(etiquetas);
 
-create rule regla_tipoetiqueta_material
+
+-- Reglas materiales
+
+create rule regla_materiales_insert
 as
 	on insert to materiales
 		where exists
 			(select * from estilos
 				where estilos.idetiqueta = new.idetiqueta)
 	do instead nothing;
-	
-create rule regla_tipoetiqueta_estilo
+
+CREATE RULE regla_materiales_update
+AS
+	ON UPDATE TO materiales
+		WHERE old.idEtiqueta != new.idEtiqueta AND EXISTS
+			(SELECT * FROM estilos
+			WHERE estilos.idEtiqueta
+			= new.idEtiqueta)
+	DO INSTEAD NOTHING;
+
+
+-- Reglas estilos
+
+CREATE RULE regla_estilos_insert
+AS
+	ON INSERT TO estilos
+		WHERE EXISTS
+			(SELECT * FROM materiales
+			WHERE materiales.idEtiqueta
+			= new.idEtiqueta)
+	DO INSTEAD NOTHING;
+
+create rule regla_estilos_update
 as
 	on update to estilos
 		where exists
@@ -35,7 +59,19 @@ as
 	do instead nothing;
 	
 	
+-- Regla para no insertar etiquetas
+
+CREATE RULE regla_insert_etiquetas
+AS
+	ON INSERT TO etiquetas
+	DO INSTEAD NOTHING;
 	
+CREATE RULE regla_update_etiquetas
+AS
+	ON INSERT TO etiquetas
+	DO INSTEAD NOTHING;
+
+
 -- INSERCION DATOS
 
 insert into etiquetas
@@ -51,11 +87,11 @@ values
 	(4,'Pastel', 12,'Similar a tiza, no necesita disolvente'),
 	(5,'Acrílico', 13,'Base de agua, secado muy rápido, acabado satinado');
 
-select * from etiquetas; -- todos
+select * from etiquetas; -- id 2 a 5
 
-select * from only etiquetas; -- solo el idEtiqueta 1
+select * from only etiquetas; -- nada
 
-select * from materiales; -- del idEtiqueta 2 a 5
+select * from materiales; -- id 2 a 5
 
 
 insert into estilos
@@ -63,19 +99,20 @@ insert into estilos
 values
 	(6,'Realista', 10, 'Alta precisión en el detalle');
 
-select * from etiquetas; -- todos
+select * from etiquetas; -- id 2 a 6
 
-select * from only etiquetas; -- solo el idEstilo 1
+select * from only etiquetas; -- nada
 
-select * from estilos; -- solo el idEstilo 6
+select * from estilos; -- id 6
+
 
 insert into estilos
 	(idetiqueta, nombre, idestilo, descripcion)
 values
 	(3,'Digital', 11,'Uso de herramientas tecnológicas');
 	
-select * from etiquetas; -- todos
+select * from etiquetas; -- id 2 a 6
 
-select * from only etiquetas; -- solo el idEstilo 1
+select * from only etiquetas; -- nada
 
-select * from estilos; -- solo el idEstilo 6
+select * from estilos; -- id 6
